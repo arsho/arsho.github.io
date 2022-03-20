880994fd1dc7b423a95066bbb2f2c1375417e9fb
arsho <shovon.sylhet@gmail.com>
2022-03-20 14:45:59 +0000

    deploy: arsho/hugo-blog@50598311bd8e3a4873960099e1470051d3915127

--- assets/js/arsho.js
+++ assets/js/arsho.js
@@ -1,1 +1,19 @@
 hljs.initHighlightingOnLoad();
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').on("click", function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+    $("#container").show();
+})
