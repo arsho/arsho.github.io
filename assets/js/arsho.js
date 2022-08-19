f9972aeba22b4d8ef2673d27ededc3e2ec7041f5
arsho <shovon.sylhet@gmail.com>
2022-08-19 23:40:26 +0000

    deploy: arsho/hugo-blog@e53bc04de08fef9217e4419e11bc5f53d0a44cf2

--- assets/js/arsho.js
+++ assets/js/arsho.js
@@ -1,4 +1,3 @@
-hljs.initHighlightingOnLoad();
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
